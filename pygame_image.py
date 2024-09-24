7cf74659f1d0335c969ccfd0ba639672d5129125
Ryoma <c0b23149f0@edu.teu.ac.jp>
2024-09-24 16:25:37 +0900

    8 OK

--- pygame_image.py
+++ pygame_image.py
@@ -13,17 +13,31 @@ def main():
     tree_img = pg.image.load("fig/3.png")
     tree_img =pg.transform.flip(tree_img,True,False)
     bg_img_1 =pg.transform.flip(bg_img,True,False)
+    tree_img_rect=tree_img.get_rect()
+    tree_img_rect.center=300,200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst=pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            tree_img_rect.move_ip((0,-1))
+        elif key_lst[pg.K_DOWN]:
+            tree_img_rect.move_ip((0,1))
+        elif key_lst[pg.K_RIGHT]:
+            tree_img_rect.move_ip((1,0))
+        elif key_lst[pg.K_LEFT]:
+            tree_img_rect.move_ip((-1,0))
         if tmr >= 3200:
             tmr=0
         screen.blit(bg_img, [-tmr, 0])
         screen.blit(bg_img_1, [1600-tmr, 0])
         screen.blit(bg_img, [3200-tmr, 0])
         screen.blit(bg_img_1, [4800-tmr, 0])
-        screen.blit(tree_img,[200,200])
+
+        screen.blit(tree_img,tree_img_rect)
+
+
         pg.display.update()
         tmr += 1
         clock.tick(1600)
