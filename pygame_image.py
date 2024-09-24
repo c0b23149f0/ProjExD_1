31f14f15cee083130df6129f8474badd07148520
Ryoma <c0b23149f0@edu.teu.ac.jp>
2024-09-24 16:08:50 +0900

    7-2made

--- pygame_image.py
+++ pygame_image.py
@@ -12,16 +12,21 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     tree_img = pg.image.load("fig/3.png")
     tree_img =pg.transform.flip(tree_img,True,False)
+    bg_img_1 =pg.transform.flip(bg_img,True,False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
-        bg_img.blit(tree_img,[300,200])
+        if tmr >= 3200:
+            tmr=0
+        screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img_1, [1600-tmr, 0])
+        screen.blit(bg_img, [3200-tmr, 0])
+        screen.blit(bg_img_1, [4800-tmr, 0])
+        screen.blit(tree_img,[200,200])
         pg.display.update()
         tmr += 1
-        clock.tick(200)
+        clock.tick(1600)
 
 
 if __name__ == "__main__":
